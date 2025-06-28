36942ecd76269d11c4ba3e992cd942c3818d3f50
Fyonietz <expduta@gmail.com>
2025-06-28 18:30:39 +0700

    init commit

---
+++ build/CMakeFiles/Ice.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "/d/Repo/Icon-Converter/Ice.exe"
+  "/d/Repo/Icon-Converter/Ice.pdb"
+  "CMakeFiles/Ice.dir/src/main.cpp.o"
+  "CMakeFiles/Ice.dir/src/main.cpp.o.d"
+  "libIce.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
